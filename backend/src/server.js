70593e7a0984692f136430f3ec3f154bd76a53fb
Sahid khan <sahidkhan1614@gmail.com>
2026-02-23 13:05:05 +0530

    Added health route

--- backend/src/server.js
+++ backend/src/server.js
@@ -19,6 +19,10 @@ require("./models/BatchMapping");
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.status(200).send("Coaching Backend Running 🚀");
+});
+
 app.use("/api/auth", require("./routes/auth.routes"));
 app.use("/api/courses", require("./routes/courseRoutes"));
 app.use("/api/visitors", require("./routes/visitorRoutes"));
