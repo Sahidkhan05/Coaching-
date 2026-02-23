a0cc75168af4371007493c096606d0d0adc88af7
Sahid khan <sahidkhan1614@gmail.com>
2026-02-23 12:57:32 +0530

    Fixed port for production

--- backend/src/server.js
+++ backend/src/server.js
@@ -36,6 +36,8 @@ app.use("/api/feedback", require("./routes/feedback.routes"));
 
 app.use("/uploads", express.static("uploads"));
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
